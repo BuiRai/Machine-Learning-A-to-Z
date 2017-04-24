9f1ed7bbc973a1e0b1bf1952edafbd909e378431
crhistian <crhistian.uc@tigears.com>
2017-04-24 14:16:20 -0500

    Feature scaling, R and Python

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -20,11 +20,15 @@ dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
 
-# # ----------  Splitting the dataset into the Training set and Test set # ---------
+# ------------  Splitting the dataset into the Training set and Test set # ---------
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 trainingSet = subset(dataset, split == TRUE)
 testSet = subset(dataset, split == FALSE)
+
+# ------------------------------ Feature Scaling -----------------------------------
+trainingSet[, 2:3] = scale(trainingSet[, 2:3])
+testSet[, 2:3] = scale(testSet[, 2:3])
 
