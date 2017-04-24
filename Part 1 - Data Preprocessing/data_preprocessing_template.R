6e80b79fec73dd7865a52ab371606c97bf3cf616
crhistian <crhistian.uc@tigears.com>
2017-04-24 12:50:44 -0500

    Encoding categorical data, R and Python

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -10,3 +10,12 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary), 
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
+
+# Encoding categorical data, 1 = France, 2 = Spain and 3 = Germany
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+# Encoding categorical data, 0 = No and 1 = Yes
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -15,3 +15,18 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Tag with numbers, 0 = France, 1 = Spain and 2 = Germany
+labelEncoder_X = LabelEncoder()
+X[:,0] = labelEncoder_X.fit_transform(X[:,0])
+# Aplying dummy variables 
+# (1,0,0) = France,
+# (0,1,0) = Germany and
+# (0,0,1) = Spain
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+# Tag with numbers, 0 = no and 1 = yes
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
