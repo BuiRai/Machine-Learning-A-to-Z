126b5c0da8761fd7859d8dd7a378b4a426e6e0ff
crhistian <crhistian.uc@tigears.com>
2017-04-24 13:42:56 -0500

    Splitting the dataset into Train and test set, R and Python

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,17 +1,17 @@
 # Data Preprocessing Template
 
-# Importing the dataset
+# -------------------------- Importing the dataset ---------------------------------
 dataset = read.csv('Data.csv')
 
-# Taking care of missing data
-dataset$Age = ifelse(is.na(dataset$Age), 
+# ------------------------- Taking care of missing data ----------------------------
+dataset$Age = ifelse(is.na(dataset$Age),
                       ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                       dataset$Age)
 dataset$Salary = ifelse(is.na(dataset$Salary), 
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
 
-# Encoding categorical data, 1 = France, 2 = Spain and 3 = Germany
+# -------Encoding categorical data, 1 = France, 2 = Spain and 3 = Germany ----------
 dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1, 2, 3))
@@ -19,3 +19,12 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
+
+# # ----------  Splitting the dataset into the Training set and Test set # ---------
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+trainingSet = subset(dataset, split == TRUE)
+testSet = subset(dataset, split == FALSE)
+
