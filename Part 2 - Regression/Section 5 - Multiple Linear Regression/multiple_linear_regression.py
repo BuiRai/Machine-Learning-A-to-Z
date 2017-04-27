5f883df47f12939e17d723a6c9b642cad6143668
buirai <buirai_naetle@hotmail.com>
2017-04-27 16:50:46 -0500

    Multiple Linear Regression in Python - Step 2

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -22,6 +22,9 @@ X[:,3] = labelEncoder_X.fit_transform(X[:,3])
 oneHotEncoder = OneHotEncoder(categorical_features = [3])
 X = oneHotEncoder.fit_transform(X).toarray()
 
+# Avoiding the Dummy variable Trap
+X = X[:, 1:]
+
 # ------- Splitting the dataset into the Training set and Test set -------
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
@@ -33,3 +36,6 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
