ef43a308bb156cb82f6dd7838ffcefb908005823
buirai <buirai_naetle@hotmail.com>
2017-04-27 16:46:51 -0500

    Multiple Linear Regression in Python - Step 1

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,35 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# --------------- Importing the dataset ---------------
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+                
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Tag with numbers, 0 = France, 1 = Spain and 2 = Germany
+labelEncoder_X = LabelEncoder()
+X[:,3] = labelEncoder_X.fit_transform(X[:,3])
+# Aplying dummy variables 
+# (1,0,0) = France,
+# (0,1,0) = Germany and
+# (0,0,1) = Spain
+oneHotEncoder = OneHotEncoder(categorical_features = [3])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+# ------- Splitting the dataset into the Training set and Test set -------
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
+                                                    random_state = 0)
+
+# ------------- Feature scaling ----------------------
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
