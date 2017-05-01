39f40a509d7cbc448f6f7257d147bca75bc8c06a
buirai <buirai_naetle@hotmail.com>
2017-05-01 00:51:08 -0500

    R Classification Template

--- Part 3 - Classification/Section 14 - Logistic Regression/classification_template.R
+++ Part 3 - Classification/Section 14 - Logistic Regression/classification_template.R
@@ -19,10 +19,8 @@ testSet = subset(dataset, split == FALSE)
 trainingSet[, 1:2] = scale(trainingSet[, 1:2])
 testSet[, 1:2] = scale(testSet[, 1:2])
 
-# Fitting Logistic Regression to the Training set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = trainingSet)
+# Fitting Classifier to the Training set
+# Create your classifier here
 
 # Predicting the Test set results
 prob_pred = predict(classifier, type = 'response', newdata = testSet[-3])
@@ -42,7 +40,25 @@ colnames(grid_set) = c('Age', 'EstimatedSalary')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, 3],
-     main = 'Logistic Regression (Training Set)',
+     main = 'Classifier (Training Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Training set results
+# install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set = testSet
+X1 = seq(min(set[, 1]) -1, max(set[, 1]) +1, by = 0.01)
+X2 = seq(min(set[, 2]) -1, max(set[, 2]) +1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, 3],
+     main = 'Classifier (Test Set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
