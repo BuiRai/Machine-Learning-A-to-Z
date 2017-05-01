f01d63ddd4dbf09b1e03ae085762b02a43e2ad84
buirai <buirai_naetle@hotmail.com>
2017-05-01 00:03:44 -0500

    Logistic Regression in R - Step 1

---
+++ Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.R
@@ -0,0 +1,18 @@
+# Logistic Regression
+
+# -------------------------- Importing the dataset ---------------------------------
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[,3:5]
+
+# ------------  Splitting the dataset into the Training set and Test set # ---------
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+trainingSet = subset(dataset, split == TRUE)
+testSet = subset(dataset, split == FALSE)
+
+# ------------------------------ Feature Scaling -----------------------------------
+trainingSet[, 1:2] = scale(trainingSet[, 1:2])
+testSet[, 1:2] = scale(testSet[, 1:2])
+
