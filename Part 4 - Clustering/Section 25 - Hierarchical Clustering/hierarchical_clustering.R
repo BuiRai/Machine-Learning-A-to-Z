91c5b3d88ae500980e5b326fef00b9296e4bb647
buirai <buirai_naetle@hotmail.com>
2017-05-09 00:35:25 -0500

    HC in R - Step 2

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.R
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.R
@@ -3,3 +3,10 @@
 # Importing the dataset
 dataset = read.csv('Mall_Customers.csv')
 X <- dataset[4:5]
+
+# Using the dendrogram to find the optimal number of clusters
+dendrogram = hclust(dist(X, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = paste('Dendrogram'),
+     xlab = 'Customers',
+     ylab = 'Euclidean distances')
