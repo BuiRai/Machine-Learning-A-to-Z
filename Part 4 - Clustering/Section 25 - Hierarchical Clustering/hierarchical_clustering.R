00e5a9863555add536d12002dbaec0298c47f364
buirai <buirai_naetle@hotmail.com>
2017-05-09 00:40:56 -0500

    HC in R - Step 4

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.R
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.R
@@ -14,3 +14,17 @@ plot(dendrogram,
 # Fitting hierarchical clustering to the mall dataset
 hc = hclust(dist(X, method = 'euclidean'), method = 'ward.D')
 y_hc = cutree(hc, 5)
+
+# Visualising the clusters
+library(cluster)
+clusplot(X,
+         y_hc, 
+         lines = 0, 
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Cluster of clients'),
+         xlab = 'Anual Income',
+         ylab = 'Spending Score')
