502e39cfd6f573e6bac1ff5e0234337f1dfbecd0
buirai <buirai_naetle@hotmail.com>
2017-05-21 23:37:24 -0500

    ANN in Python - Step 2

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 # --------------- Importing the dataset ---------------
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:12].values
+X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # --------------- Encoding categorical data -----------
@@ -41,3 +41,41 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6 , use_bias=True, kernel_initializer='glorot_uniform',
+                       bias_initializer='zeros', activation='relu' , input_dim =11) )
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+accuracy = (cm[0][0] + cm[1][1]) / 2000
